969e2d46027ac5172ad13e0dd0d43632245176b9
deployhausgold <deploy@hausgold.de>
2024-08-09 14:10:42 +0000

    [Release] Bumped the version to 1.2.2.

--- lib/price_hubble/version.rb
+++ lib/price_hubble/version.rb
@@ -3,7 +3,7 @@
 # The gem version details.
 module PriceHubble
   # The version of the +price-hubble+ gem
-  VERSION = '1.2.1'
+  VERSION = '1.2.2'
 
   class << self
     # Returns the version of gem as a string.
